e9585faafa02e8309cb85729c81bf4a3ab56fb94
Wool5443 <mihsolodilov2015@gmail.com>
2024-05-21 12:05:19 +0300

    Now generates colors using SDL_MapRGBA and draws grid

--- headers/GameDrawer.hpp
+++ headers/GameDrawer.hpp
@@ -4,14 +4,11 @@
 #include "GameOfLife.hpp"
 #include "Utils.hpp"
 
-constexpr int CELL_SIZE = 10;
+constexpr int CELL_SIZE       = 20;
+constexpr int GRID_LINE_WIDTH = 1;
 
-constexpr char ALPHA = 24;
-constexpr char BLUE  =  0;
-constexpr char GREEN =  8;
-constexpr char RED   = 16;
-
-constexpr uint32_t LIVE_CELL_COLOR = (0xff << RED) + (0xff << ALPHA);
-constexpr uint32_t DEAD_CELL_COLOR = (0xff << ALPHA);
+void InitDrawing(SDL_PixelFormat* format);
 
 void GameDraw(SDL_Surface* surface, const Game& game);
+
+void DrawGrid(SDL_Surface* surface);
--- src/GameDrawer.cpp
+++ src/GameDrawer.cpp
@@ -1,6 +1,17 @@
 #include "GameDrawer.hpp"
 
-void _drawCell(SDL_Surface* surface, char value, int row, int col);
+static uint32_t LIVE_CELL_COLOR  = 0;
+static uint32_t BACKGROUND_COLOR = 0;
+static uint32_t GRID_COLOR       = 0;
+
+static void _drawCell(SDL_Surface* surface, char value, int row, int col);
+
+void InitDrawing(SDL_PixelFormat* format)
+{
+    BACKGROUND_COLOR = SDL_MapRGBA(format, 0x00, 0x00, 0x00, 0xff);
+    LIVE_CELL_COLOR  = SDL_MapRGBA(format, 0xff, 0x00, 0x00, 0xff);
+    GRID_COLOR       = SDL_MapRGBA(format, 0x60, 0x60, 0x60, 0xff);
+}
 
 void GameDraw(SDL_Surface* surface, const Game& game)
 {
@@ -15,6 +26,35 @@ void GameDraw(SDL_Surface* surface, const Game& game)
     }
 }
 
+void DrawGrid(SDL_Surface* surface)
+{
+    MyAssertHard(surface, ERROR_NULLPTR);
+
+    int linesCountV = surface->w / CELL_SIZE;
+    int linesCountH = surface->h / CELL_SIZE;
+
+    SDL_Rect* lines = new SDL_Rect[linesCountV + linesCountH];
+    if (!lines)
+    {
+        PRINT_ERROR(ERROR_NO_MEMORY);
+        return;
+    }
+
+    for (int x = -1, i = 0; i < linesCountV; i++, x += CELL_SIZE)
+        lines[i] = { .x = x, .y = 0, .w = GRID_LINE_WIDTH, .h = surface->h };
+    
+
+    for (int y = -1, i = linesCountV; i < linesCountV + linesCountH; i++, y += CELL_SIZE)
+        lines[i] = { .x = 0, .y = y, .w = surface->w, .h = GRID_LINE_WIDTH };
+
+    SDL_LockSurface(surface);
+    SDL_FillRect(surface, nullptr, BACKGROUND_COLOR);
+    SDL_FillRects(surface, lines, linesCountV + linesCountH, GRID_COLOR);
+    SDL_UnlockSurface(surface);
+
+    delete[] lines;
+}
+
 static SDL_Rect _cellRect = { .x = 0, .y = 0, .w = CELL_SIZE - 1, .h = CELL_SIZE - 1 };
 void _drawCell(SDL_Surface* surface, char value, int row, int col)
 {
@@ -26,7 +66,7 @@ void _drawCell(SDL_Surface* surface, char value, int row, int col)
     _cellRect.x = x;
     _cellRect.y = y;
 
-    uint32_t color = value == ALIVE ? LIVE_CELL_COLOR : DEAD_CELL_COLOR;
+    uint32_t color = value == ALIVE ? LIVE_CELL_COLOR : BACKGROUND_COLOR;
 
     SDL_LockSurface(surface);
     SDL_FillRect(surface, &_cellRect, color);
